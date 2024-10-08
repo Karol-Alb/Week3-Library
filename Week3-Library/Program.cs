9254ebf74cb584510e8161ffb80c7206f1b34abc
csf23alk <csf23alk@MF124PC24>
2024-10-08 09:41:57 +0100

    update

--- Week3-Library/Program.cs
+++ Week3-Library/Program.cs
@@ -6,6 +6,15 @@
 
     static void Main(string[] args)
     {
+        // Create a new instance for the book class
         Book book = new Book();
+        book.Title = "C# for beginners";
+        book.Author = "Bill Gates";
+        book.ISBN = "12356677";
+
+        // Output the book information to the console
+        Console.WriteLine($"book title; {book.Title}");
+        Console.WriteLine($"Book author: {book.Author}");
+        Console.WriteLine($"Book ISBn: {book.ISBN}");
     }
 }
