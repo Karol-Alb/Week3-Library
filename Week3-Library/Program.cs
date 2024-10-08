7909d889067001f6ac49977d061e8b37b9d9fb0b
csf23alk <csf23alk@MF124PC24>
2024-10-08 09:50:36 +0100

    update

--- Week3-Library/Program.cs
+++ Week3-Library/Program.cs
@@ -12,9 +12,22 @@
         book.Author = "Bill Gates";
         book.ISBN = "12356677";
 
+        Book book2 = new Book();
+        book2.Title = "C# methods and classes";
+        book2.Author = "Microsoft";
+        book2.ISBN = "1234566";
+
         // Output the book information to the console
-        Console.WriteLine($"book title; {book.Title}");
+        Console.WriteLine($"book title: {book.Title}");
         Console.WriteLine($"Book author: {book.Author}");
         Console.WriteLine($"Book ISBn: {book.ISBN}");
+
+        // Output second book information
+        Console.WriteLine($"booke title: {book2.Title}");
+        Console.WriteLine($"book author: {book2.Author}");
+        Console.WriteLine($"book ISBN: {book2.ISBN}");
+
+
+
     }
 }
