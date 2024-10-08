cd70fb2683b187d83c1e986a55d22266a3dd2fe3
csf23alk <csf23alk@MF124PC24>
2024-10-08 09:34:37 +0100

    update

--- Week3-Library/Program.cs
+++ Week3-Library/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Book
+{
+    string Title;
+    string Author;
+    string ISBN;
+
+    static void Main(string[] args)
+    {
+        Book book = new Book();
+    }
+}
